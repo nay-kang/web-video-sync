51c5c13ace3f88ad8fdcef76218924ffb95bec32
nay <kunvunk@gmail.com>
2020-04-12 21:04:48 +0800

    first run

---
+++ chatserver.js
@@ -0,0 +1,262 @@
+//#!/usr/bin/env node
+//
+// WebSocket chat server
+// Implemented using Node.js
+//
+// Requires the websocket module.
+//
+// WebSocket and WebRTC based multi-user chat sample with two-way video
+// calling, including use of TURN if applicable or necessary.
+//
+// This file contains the JavaScript code that implements the server-side
+// functionality of the chat system, including user ID management, message
+// reflection, and routing of private messages, including support for
+// sending through unknown JSON objects to support custom apps and signaling
+// for WebRTC.
+//
+// Requires Node.js and the websocket module (WebSocket-Node):
+//
+//  - http://nodejs.org/
+//  - https://github.com/theturtle32/WebSocket-Node
+//
+// To read about how this sample works:  http://bit.ly/webrtc-from-chat
+//
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+"use strict";
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var WebSocketServer = require('websocket').server;
+
+// Pathnames of the SSL key and certificate files to use for
+// HTTPS connections.
+
+const keyFilePath = "/etc/pki/tls/private/mdn-samples.mozilla.org.key";
+const certFilePath = "/etc/pki/tls/certs/mdn-samples.mozilla.org.crt";
+
+// Used for managing the text chat user list.
+
+var connectionArray = [];
+var nextID = Date.now();
+
+// Output logging information to console
+
+function log(text) {
+  var time = new Date();
+
+  console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
+
+// If you want to implement support for blocking specific origins, this is
+// where you do it. Just return false to refuse WebSocket connections given
+// the specified origin.
+function originIsAllowed(origin) {
+  return true;    // We will accept all connections
+}
+
+function sendToRoom(self,target,msgString){
+  console.log("send to room",self,target)
+  for (var i=0;i<connectionArray.length;i++){
+    if(connectionArray[i].room_id===target && connectionArray[i].clientID!==self){
+      console.log("send to room user",connectionArray[i].clientID)
+      connectionArray[i].sendUTF(msgString)
+    }
+  }
+}
+// Scan the list of connections and return the one for the specified
+// clientID. Each login gets an ID that doesn't change during the session,
+// so it can be tracked across username changes.
+function getConnectionForID(id) {
+  var connect = null;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].clientID === id) {
+      connect = connectionArray[i];
+      break;
+    }
+  }
+
+  return connect;
+}
+
+
+// Try to load the key and certificate files for SSL so we can
+// do HTTPS (required for non-local WebRTC).
+
+var httpsOptions = {
+  key: null,
+  cert: null
+};
+
+try {
+  httpsOptions.key = fs.readFileSync(keyFilePath);
+  try {
+    httpsOptions.cert = fs.readFileSync(certFilePath);
+  } catch(err) {
+    httpsOptions.key = null;
+    httpsOptions.cert = null;
+  }
+} catch(err) {
+  httpsOptions.key = null;
+  httpsOptions.cert = null;
+}
+
+// If we were able to get the key and certificate files, try to
+// start up an HTTPS server.
+
+var webServer = null;
+
+try {
+  if (httpsOptions.key && httpsOptions.cert) {
+    webServer = https.createServer(httpsOptions, handleWebRequest);
+  }
+} catch(err) {
+  webServer = null;
+}
+
+if (!webServer) {
+  try {
+    webServer = http.createServer({}, handleWebRequest);
+  } catch(err) {
+    webServer = null;
+    log(`Error attempting to create HTTP(s) server: ${err.toString()}`);
+  }
+}
+
+
+// Our HTTPS server does nothing but service WebSocket
+// connections, so every request just returns 404. Real Web
+// requests are handled by the main server on the box. If you
+// want to, you can return real HTML here and serve Web content.
+
+function handleWebRequest(request, response) {
+  log ("Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
+}
+
+// Spin up the HTTPS server on the port assigned to this sample.
+// This will be turned into a WebSocket port very shortly.
+
+webServer.listen(6503, function() {
+  log("Server is listening on port 6503");
+});
+
+// Create the WebSocket server by converting the HTTPS server into one.
+
+var wsServer = new WebSocketServer({
+  httpServer: webServer,
+  autoAcceptConnections: false
+});
+
+if (!wsServer) {
+  log("ERROR: Unable to create WbeSocket server!");
+}
+
+// Set up a "connect" message handler on our WebSocket server. This is
+// called whenever a user connects to the server's port using the
+// WebSocket protocol.
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    log("Connection from " + request.origin + " rejected.");
+    return;
+  }
+
+  // Accept the request and get a connection.
+
+  var connection = request.accept("json", request.origin);
+
+  // Add the new connection to our list of connections.
+
+  log("Connection accepted from " + connection.remoteAddress + ".");
+  connectionArray.push(connection);
+
+  connection.clientID = nextID;
+  nextID++;
+
+  // Send the new client its token; it send back a "username" message to
+  // tell us what username they want to use.
+
+  var msg = {
+    type: "id",
+    id: connection.clientID
+  };
+  connection.sendUTF(JSON.stringify(msg));
+
+  // Set up a handler for the "message" event received over WebSocket. This
+  // is a message sent by a client, and may be text to share with other
+  // users, a private message (text or signaling) for one user, or a command
+  // to the server.
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      log("Received Message: " + message.utf8Data);
+
+      // Process incoming data.
+
+      var sendToClients = true;
+      msg = JSON.parse(message.utf8Data);
+      var connect = getConnectionForID(msg.id);
+
+      // Take a look at the incoming object and act on it based
+      // on its type. Unknown message types are passed through,
+      // since they may be used to implement client-side features.
+      // Messages with a "target" property are sent only to a user
+      // by that name.
+
+      switch(msg.type) {
+        // Public, textual message
+        case "message":
+          msg.room_id = connect.room_id;
+          msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
+          break;
+
+        // Username change
+        case "room_id":
+          connect.room_id = msg.room_id;
+          sendToClients = false;  // We already sent the proper responses
+          break;
+      }
+
+      // Convert the revised message back to JSON and send it out
+      // to the specified client or all clients, as appropriate. We
+      // pass through any messages not specifically handled
+      // in the select block above. This allows the clients to
+      // exchange signaling and other control objects unimpeded.
+
+      if (sendToClients) {
+        var msgString = JSON.stringify(msg);
+        sendToRoom(msg.id,msg.room_id,msgString);
+      }
+    }
+  });
+
+  // Handle the WebSocket "close" event; this means a user has logged off
+  // or has been disconnected.
+  connection.on('close', function(reason, description) {
+    // First, remove the connection from the list of connections.
+    connectionArray = connectionArray.filter(function(el, idx, ar) {
+      return el.connected;
+    });
+
+    // Now send the updated user list. Again, please don't do this in a
+    // real application. Your users won't like you very much.
+    // sendUserListToAll();
+
+    // Build and output log output for close information.
+
+    var logMessage = "Connection closed: " + connection.remoteAddress + " (" +
+                     reason;
+    if (description !== null && description.length !== 0) {
+      logMessage += ": " + description;
+    }
+    logMessage += ")";
+    log(logMessage);
+  });
+});
